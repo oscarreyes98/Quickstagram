170b53f9c4c5cd843be6af8e2e8c138cf7ed22bc
Oscar Reyes <oxreyes@ucsd.edu>
2017-03-08 15:31:36 -0800

    Initial Commit

---
+++ Quickstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quickstagram
+//
+//  Created by Oscar Reyes on 3/8/17.
+//  Copyright © 2017 Oscar Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
