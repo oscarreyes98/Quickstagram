fd3114e8a88c63c09ac7c2687273955a25f94b39
Oscar Reyes <oxreyes@ucsd.edu>
2017-03-12 20:14:34 -0700

    new update -

---
+++ Quickstagram/CaptureViewController.swift
@@ -0,0 +1,107 @@
+//
+//  CaptureViewController.swift
+//  Quickstagram
+//
+//  Created by Oscar Reyes on 3/11/17.
+//  Copyright © 2017 Oscar Reyes. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class CaptureViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var captionTextField: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectPhoto(tapGestureRecognizer:)))
+        pictureImageView.addGestureRecognizer(tapGestureRecognizer)
+        pictureImageView.isUserInteractionEnabled = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            pictureImageView.image = pickedImage
+            
+           /* if self.pictureImageView.image != nil {
+                self.addImageLabel.isHidden = true
+            }
+            else {
+                self.addImageLabel.isHidden = false
+            }*/
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func selectPhoto(tapGestureRecognizer: UITapGestureRecognizer) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        if self.pictureImageView.image == nil {
+            print("No photo was selected, could not submit")
+            return
+        }
+        
+        let imageToPost = resize(image: self.pictureImageView.image!, newSize: CGSize(width: 200, height: 200))
+        let captionToPost = self.captionTextField.text
+        
+        Post.postUserImage(image: imageToPost, withCaption: captionToPost) { (success: Bool, error: Error?) in
+            if success {
+                print("Submitted photo")
+                self.tabBarController?.selectedIndex = 0
+                self.pictureImageView.image = nil
+                self.captionTextField.text = ""
+                //self.addImageLabel.isHidden = false
+            }
+            else {
+                print(error?.localizedDescription ?? "Error submitting photo")
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Quickstagram/HomeViewController.swift
@@ -0,0 +1,81 @@
+//
+//  HomeViewController.swift
+//  Quickstagram
+//
+//  Created by Oscar Reyes on 3/11/17.
+//  Copyright © 2017 Oscar Reyes. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var postsTableView: UITableView!
+    var posts: [PFObject]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.postsTableView.dataSource = self
+        self.postsTableView.delegate = self
+        self.postsTableView.rowHeight = UITableViewAutomaticDimension
+        self.postsTableView.estimatedRowHeight = 140
+        
+        let query = PFQuery(className: "Post")
+        
+        // Fetch data asynchronously
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) -> Void in
+            if let posts = posts {
+                self.posts = posts
+                self.postsTableView.reloadData()
+            } else {
+                print(error?.localizedDescription ?? "Error getting images")
+            }
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (posts?.count) ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pictureCell", for: indexPath) as! PictureCell
+        let post = posts?[indexPath.row]
+        
+        let photo = post?["media"] as! PFFile
+        photo.getDataInBackground {(data: Data?, error: Error?) in
+            if error == nil {
+                cell.picImageView.image = UIImage(data: data!)
+            }
+        }
+        
+        cell.captionLabel.text = post?["caption"] as? String
+        
+        cell.backgroundColor = UIColor.cyan
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
